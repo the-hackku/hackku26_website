41761555ec428894d0b8eef7b5a9a14357d82fc8
Aniketh A. <aaatipamula@gmail.com>
2026-03-02 10:40:14 -0600

    update: ✅ add mlh code of conduct to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,34 +21,49 @@ export default function Footer() {
     <footer className="bg-gray-100 text-gray-600 py-6 w-full">
       <div className="container mx-auto px-4 flex justify-between items-center">
         {/* Social Links on the left */}
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 text-gray-500">
           <Link
             href={constants.discordInvite}
             target="_blank"
-            className="text-gray-400 hover:text-blue-400 transition-colors"
+            rel="noopener noreferrer"
+            className="hover:text-blue-400 transition-colors"
           >
             <IconBrandDiscord size={20} />
           </Link>
           <Link
             href="https://www.instagram.com/thehackku/"
             target="_blank"
-            className="text-gray-400 hover:text-pink-600 transition-colors"
+            className="hover:text-pink-600 transition-colors"
           >
             <IconBrandInstagram size={20} />
           </Link>
           <Link
             href="https://www.linkedin.com/company/hackku/about/"
             target="_blank"
-            className="text-gray-400 hover:text-blue-700 transition-colors"
+            rel="noopener noreferrer"
+            className="hover:text-blue-700 transition-colors"
           >
             <IconBrandLinkedin size={20} />
           </Link>
           <Link
             href="https://github.com/the-hackku/hackku26_website"
             target="_blank"
-            className="text-gray-400 hover:text-gray-900 transition-colors"
+            rel="noopener noreferrer"
+            className="hover:text-gray-900 transition-colors"
           >
             <IconBrandGithub size={20} />
+          </Link>
+        </div>
+
+        {/* Center text */}
+        <div className="text-gray-400 underline underline-offset-2" >
+          <Link
+            href="/mlh/code-of-conduct"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mr-1"
+          >
+            MLH Code of Conduct
           </Link>
         </div>
 
