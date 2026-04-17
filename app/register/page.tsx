a471b1c6c5e5ce77d77b59a4feeb1bb8c6fab3f3
Aniketh A. <aaatipamula@gmail.com>
2026-04-17 13:54:35 -0500

    update: ✅ new registration cutoff date

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,11 +3,14 @@ import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/authoptions";
 import { prisma } from "@/lib/prisma";
 import { RegistrationForm } from "@/components/forms/RegistrationForm";
-import constants from "@/constants";
+// import constants from "@/constants";
+
+// const REGISTRATION_CUTOFF = new Date(constants.startDate);
+const REGISTRATION_CUTOFF = new Date("Sat Apr 18 12:00:00 PM CDT 2026");
 
 export default async function RegisterPage() {
   // Close registration past checkin
-  if (new Date() >= new Date(constants.startDate)) {
+  if (new Date() >= REGISTRATION_CUTOFF) {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <p>Registration is closed, please come back for HackKU next year!</p>
