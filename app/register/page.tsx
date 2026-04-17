38c78d3b6a07494289b11b8f3d9213fd9e264a6f
Aniketh A. <aaatipamula@gmail.com>
2026-04-17 10:52:27 -0500

    feat: ✨ close registration past date

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,20 +1,20 @@
-// export default async function RegisterPage() {
-//   // If not registered, show the registration form
-//   return (
-//     <div className="flex flex-col items-center justify-center h-screen">
-//       <p>Registration is closed</p>
-//       <p>HackKU26 coming soon!</p>
-//     </div>
-//   );
-// }
-
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/authoptions";
 import { prisma } from "@/lib/prisma";
 import { RegistrationForm } from "@/components/forms/RegistrationForm";
+import constants from "@/constants";
 
 export default async function RegisterPage() {
+  // Close registration past checkin
+  if (new Date() >= new Date(constants.startDate)) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <p>Registration is closed, please come back for HackKU next year!</p>
+      </div>
+    );
+  }
+
   const session = await getServerSession(authOptions);
 
   // If the user is not authenticated, redirect to the signin page
